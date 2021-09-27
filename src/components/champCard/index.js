2aaff9851ad1c0ee872f92fabceffef3256b2f69
Nolan Stucky <nolan@stucky.net>
2021-09-27 15:54:38 -0700

    added logic to be able to click on champion and remove it from champions array

--- src/components/champCard/index.js
+++ src/components/champCard/index.js
@@ -1,34 +1,51 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import { Stack } from '@mui/material';
 
+
 export default function ChampCard(props) {
 
+    const [showChamp, setShowChamp] = useState(true);
+   
+
+    function handleClick(e) {
+        const id = e.target.id;
+        let playedChampions = JSON.parse(localStorage.getItem("played champ"));
+        playedChampions.push(id)
+        localStorage.setItem('played champ', JSON.stringify(playedChampions));
+        setShowChamp(false)
+        console.log(id);
+      }
+
     return (
-        <Stack margin="5px">
-            <Box sx={{
-                
-            }}>
-            
-                <img src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.props}_0.jpg`} height="225px"/>
-            </Box>
-            <Box sx={{
-                width: "123px",
-                height: "50px",
-                zIndex:1,
-                bgcolor: 'black',
-                marginTop: -55,
-            }}>
+        <Stack margin="5px" onClick={handleClick} id={props.props}>
+            {showChamp ? 
+            <Box>
                 <Box sx={{
-                    marginBottom: 5,
+                    
                 }}>
-                    <h1 style={{fontSize:"22px", marginTop:"5px", marginLeft:"10px", color:"white", fontFamily:"Friz Quadrata"}}>{props.props}</h1>
-                   
+                
+                    <img id={props.props} src={`http://ddragon.leagueoflegends.com/cdn/img/champion/loading/${props.props}_0.jpg`} height="225px"/>
+                </Box>
+                <Box sx={{
+                    width: "124px",
+                    height: "50px",
+                    zIndex:1,
+                    bgcolor: 'black',
+                    marginTop: -55,
+                }}>
+                    <Box sx={{
+                        marginBottom: 5,
+                    }}>
+                        <h1 style={{fontSize:"22px", marginTop:"5px", marginLeft:"10px", color:"white", fontFamily:"Friz Quadrata"}}>{props.props}</h1>
+                    
+                    </Box>
                 </Box>
             </Box>
+            : null}
         </Stack>
 
 
--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,26 +1,20 @@
-import * as React from 'react';
+import { React, useEffect, useState, useRef } from 'react'
 import { styled } from '@mui/material/styles';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-
-const StyledToolbar = styled(Toolbar)(({ theme }) => ({
-  alignItems: 'flex-start',
-  paddingTop: theme.spacing(1),
-  paddingBottom: theme.spacing(2),
-  // Override media queries injected by theme.mixins.toolbar
-  '@media all': {
-    minHeight: 128,
-  },
-}));
+function handleClick(e) {
+    e.preventDefault();
+    console.log('The link was clicked.');
+  }
 
 export default function Navbar() {
   return (
     <Box sx={{ flexGrow: 1 }} style={{ position: "fixed", top: 0, width: "100%", height: "100px", backgroundColor: "#121c40", zIndex: 10, boxShadow: "0 5px 8px 0 rgba(0, 0, 0, 0.9), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"}} boxShadow="initial">
-
+        <Button onClick={handleClick} size="500px">Three</Button>
     </Box>
   );
 }
